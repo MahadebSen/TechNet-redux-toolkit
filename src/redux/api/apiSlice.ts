ead14de8e78337293bababcef973978a78a0c96e
Mahadeb Sen <mahadebsen237@gmail.com>
2024-02-25 00:57:55 +0530

    fix: the backend endpoint has been changed

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://technet-server-production.up.railway.app/',
+    baseUrl: 'https://technet-server-y8zu.onrender.com/',
   }),
   tagTypes: ['comments'],
   endpoints: () => ({}),
