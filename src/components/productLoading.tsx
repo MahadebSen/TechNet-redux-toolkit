f5f1907e3b68dbf838ce8778e7e6084882689089
Mahadeb Sen <mahadebsen237@gmail.com>
2024-06-10 01:21:58 +0530

    Loading skeleton color has been changed successfully.

--- src/components/productLoading.tsx
+++ src/components/productLoading.tsx
@@ -4,11 +4,31 @@ import 'react-loading-skeleton/dist/skeleton.css';
 export default function ProductLoading() {
   return (
     <div className="md:col-span-9 grid md:grid-cols-2 lg:grid-cols-3 gap-10 pb-20">
-      <Skeleton className="h-96 p-0 mx-auto" />
-      <Skeleton className="h-96 p-0 mx-auto" />
-      <Skeleton className="h-96 p-0 mx-auto" />
-      <Skeleton className="h-96 p-0 mx-auto" />
-      <Skeleton className="h-96 p-0 mx-auto" />
+      <Skeleton
+        baseColor="#f2f0f2"
+        highlightColor="#c5c3c3"
+        className="h-96 p-0 mx-auto"
+      />
+      <Skeleton
+        baseColor="#f2f0f2"
+        highlightColor="#c5c3c3"
+        className="h-96 p-0 mx-auto"
+      />
+      <Skeleton
+        baseColor="#f2f0f2"
+        highlightColor="#c5c3c3"
+        className="h-96 p-0 mx-auto"
+      />
+      <Skeleton
+        baseColor="#f2f0f2"
+        highlightColor="#c5c3c3"
+        className="h-96 p-0 mx-auto"
+      />
+      <Skeleton
+        baseColor="#f2f0f2"
+        highlightColor="#c5c3c3"
+        className="h-96 p-0 mx-auto"
+      />
     </div>
   );
 }
